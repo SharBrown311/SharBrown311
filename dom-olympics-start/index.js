6496d3750b653884f267894e8aee3d87d6120ec8
Sharon Brown <sbsharonbrown311@gmail.com>
2022-08-17 12:40:42 -0400

    Dom Olympics Start Assignment

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -1,92 +1,90 @@
-var h1 = document.createElement("h1"),
-        h4 = document.createElement('h4'),
-            h3 = document.createElement('h3');
+var h1 = document.createElement("h1");
+      var spanName = document.createElement('span')
+        // spanName.setAttribute ('class', 'spanName');//doesn't work
+        var h2 = document.createElement('h2');
 
-h1.textContent = ("JavaScript Made this!!"),
-    h4.textContent = ("Sharon"),
-        h3.textContent = ("wrote the JavaScript");
 
-        document.getElementById("header").appendChild(h1);
-            h1.className = "header";
-                header.appendChild(h4);
-                header.appendChild(h3);
-                
-                
+var header = document.getElementById("header")
+    header.appendChild(h1).textContent = "JavaScript made this!!"
+    header.appendChild(h2).textContent = ' made the JavaScript'
+    h2.prepend(span = 'Sharon')
+    
+    
                 h1.style.display = "inline-flex";
                 h1.style.position = "relative";
                 h1.style.paddingLeft = "34%";
-                h4.style.position = "absolute";
-                h4.style.fontWeight = "bold"
-                h4.style.fontSize = '23px'
-                h4.style.display = 'flex';
-                h4.style.paddingLeft = "34.5%";
-                h4.style.marginTop = "1%";
-                h4.style.paddingTop = "-20%";
-                h4.style.alignItems = 'flex';
-                h3.style.display = "flex";
-                h3.style.fontSize = "23px";
-                h3.style.paddingLeft = "39.5%";
-                h3.style.marginTop =  "1%";
-                h3.style.alignItems = "flex";
-                h3.style.position = "absolute";
+                // h2.spanName.style.color = "orange"//-doesn't work
 
 
-             
-const messages = document.getElementsByClassName('messages')[0].children;
-    console.log(document.getElementsByClassName('messages'))
+//array of messages to replace with  new text content 
+    const messages = document.getElementsByClassName('messages')[0].children;
     messages[0].textContent = 'My wife accused me of being immature today';
     messages[1].textContent  = 'Really? What did you say to her?';
     messages[2].textContent = 'I told her to get out of my fort!!!!';
     messages[3].textContent = "Seriously, no girls allowed!";
-    
 
+//button to clear out contents of messages - after hitting
+//send all content reappears
 var clearButton = document.getElementById('clear-button')
 
-clearButton.addEventListener('click', function(event){
-  clearContent()  
-})
+clearButton.addEventListener('click', (event) => {
+        clearContent();
+    })
     function clearContent(){
      messages[0].textContent = "";
      messages[1].textContent = "";
      messages[2].textContent = "";
      messages[3].textContent = "";
     }
+var brownBlue = true;
+var redBlack = false;
+var greenGold = false;
+var yellowOrange = false;
+var select = document.getElementById('themeDropDown')
+     themeDropDown.addEventListener('click' , function(e){
+            var messageContainer = document.querySelector('#main')
+            var messages = document.querySelectorAll('.messages')
+            var messageRight = document.querySelectorAll('.messageRight')
+            var messageLeft = document.querySelectorAll('.messageLeft')
+                // const listItems = 
+                // [listItems[0].messages.style.backgroundColor = 'brown',
+                //  listItems[1].messages.style.backgroundColor = 'red',
+                //  listItems[2].messages.style.backgroundColor = 'gold',
+                //  listItems[3].messages.style.backgroundColor = 'orange'
+                // ]
+            if (brownBlue = true) {
+                messageRight.style.backgroundColor = 'brown'
+                messageLeft.style.backgroundColor = 'blue'
+                brownBlue = true;
+                redBlack = false;
+                greenGold = false;
+                yellowOrange = false;
+            }else if(redBlack = true) {
+                messageRight.style.backgroundColor = 'black'
+                messageRight.style.color = 'white'
+                messageLeft.style.backgroundColor = 'red'
+                brownBlue = false;
+                redBlack = true;
+                greenGold = false;
+                yellowOrange = false; 
+            }else if(greenGold = true){
+                messageRight.style.backgroundColor = 'green'
+                messageLeft.style.backgroundColor = 'gold'
+                brownBlue = false;
+                redBlack = false;
+                greenGold = true;
+                yellowOrange = false;
+            }else if(yellowOrange = true){
+                messageRight.style.backgroundColor = 'yellow'
+                messageLeft.style.backgroundColor = 'orange'
+                brownBlue = false;
+                redBlack = false;
+                greenGold = false;
+                yellowOrange = true;
+            }
+    })
 
-var select =document.getElementById('themeDropDown');
-    console.log(themeDropDown)
+   
 
 
-
-
-
-
-
-
-
-
-
-
-
-// var select = document.getElementById('themeDropDown');
-// var option = document.getElementsByTagName("option");
-// var value = document.getElementsByTagName("themeOne", "themeTwo").children;
-//     var value = document.getElementById('listItemOne', 'listItemTwo');
-//       var onchange = document.getElementById('listItemOne') 
-//       listItemOne.addEventListener('onchange', function(event){
-//          onchange()   
-//             listItemOne[0].messages.style.backgroundColor = 'blue', 'brown';
-      
-//       }
-//       )
-
-//     var onchange =document.getElementById('listItemTwo')
-//     listItemTwo.addEventListener('onchange', function(event){
-//         onchange()
-//         listItemTwo[1].messages.style.backgroundColor = 'red' , 'black';
-//     }
-    
-// for themeDropDown
-
-            
-            
-    // )
+//
