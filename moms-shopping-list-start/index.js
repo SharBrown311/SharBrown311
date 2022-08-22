b7d587e564fb6a293cfbf81b73b0c1677de4d73a
Sharon Brown <sbsharonbrown311@gmail.com>
2022-08-22 16:13:59 -0400

    Added Moms Shopping List

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,18 +1,64 @@
-//Adding Element Selection
-// div.title-bar : className ='title-bar' , value = 'title-bar
-   //child of div/title-bar //h3.title
-   console.log(document.all)
+var button = document.getElementById('submitItem')
+submitItem.innerHTML = 'Submit'//submit button
+var list = document.getElementById('list')//ul
 
+                                                            
+var form = document.getElementById('listForm')
+// 1. console.log contents of variables that store returns from dom manipulation 
+console.log("form var: ", form)
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    addItem()
+})//form element
 
-
-
-
-
-
-
-//save();{
-//     if (localStorage.items == undefined) {
-//         localStorage.items = localStorage.items 
-//         = JSON.stringify('list.items');
-//     }
+// var testFn = function() {
+//     // e.preventDefault()
+//     console.log("The form was submitted")
 // }
+
+// var exampleForm = document.getElementById("exampleForm")
+// console.log(exampleForm)
+
+var addItem = function () {
+
+    var text = txtValu.value;
+    txtValu.value = "";
+    var li = document.createElement('li');
+    li.innerHTML = text;
+    var br = document.createElement('br')
+    list.appendChild(li);
+    li.append(br)
+
+    var buttonDelete = document.createElement('button')
+    buttonDelete.addEventListener('click' , deleteItem)
+
+    // named function definition
+    function deleteItem(e){
+        console.log("delete was called")
+        li.remove()
+    }
+    var buttonEdit = document.createElement('button')
+    buttonDelete.innerHTML = "X"
+    buttonEdit.innerHTML = 'Edit'
+    buttonDelete.style.height= '30px'
+        buttonDelete.style.width = '43.09px'
+        buttonDelete.style.backgroundColor = 'red'
+        buttonEdit.style.height = '30px'
+        buttonEdit.style.width = '43.09px'
+        buttonEdit.style.backgroundColor = 'blue'
+    li.appendChild(buttonDelete)
+    li.appendChild(buttonEdit)
+}
+// //remove list item
+
+
+
+//     //edit value of input and be able to save afterwards
+// // var editItem = document.querySelector('editItem').addEventListener('click' , update)
+
+// // var update = function(){
+// //     var txtValu = document.getElementById('txtValu')
+// //     var listItem = document.getElementById (txtValu.value)
+
+// // }
+
