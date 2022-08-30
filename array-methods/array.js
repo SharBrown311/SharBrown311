a8c5edcbf7ef499a4b5258f928e33a1a41d01ebb
Sharon Brown <sbsharonbrown311@gmail.com>
2022-08-30 10:46:59 -0400

    Forception Assignment

--- array-methods/array.js
+++ array-methods/array.js
@@ -1,16 +1,37 @@
-var fruit = ['banana', 'apple', 'orange', 'watermelon'];
+var fruits = ['banana', 'apple', 'orange', 'watermelon'];
 var vegetables = ['carrot' , 'tomato' , 'pepper' , 'lettuce'];
 
-var poppedItem = vegetables.pop();
-console.log(poppedItem);
-//output removed last index item lettuce
-console.log("vegetables: ", vegetables)
-//output: "vegetables: " // [object Array] (3)
-//["carrrot","tomato","pepper"]
+var vegetables = vegetables.pop();
+console.log("vegetables: ", vegetables);
+//output: "vegetables: " "lettuce"
 
-var firstElement =  fruit.shift()
-console.log(firstElement)
-//output : banana
+var fruits = fruits.shift();
+console.log("fruit: " , fruit);
+//output: "fruit: " "banana"
 
-var orangeIndex = fruit.indexOf("orange", fruit)
-console.log(orangeIndex)
+var fruits = fruits.indexOf("orange", fruits)
+console.log(fruitsSecondChange)
+//output "fruits: 1
+
+var vegetablesSecondChange = vegetables.length
+console.log(vegetablesSecondChange)
+//output:3
+var vegetablesThirdChange = vegetables.push(3)
+console.log(vegetables)
+//output:// [object Array] (4)
+//["carrot","tomato","pepper",3]
+var food = fruits.concat(vegetables)
+console.log(food)
+//output: // [object Array] (8)
+//["apple","orange","watermelon",1,"carrot","tomato","pepper",3]
+var foodTwo = food.splice(4 , 2)
+console.log(foodTwo)
+//output: // [object Array] (2)
+//["carrot","tomato"]
+var food = food.reverse()
+console.log(food)
+//output: // [object Array] (6)
+//[3,"pepper",1,"watermelon","orange","apple"]
+var food = food.join();
+console.log(food)
+//output: "3,pepper,1,watermelon,orange,apple"
---
+++ forception/forcept.js
@@ -0,0 +1,14 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var allChar = alphabet.split('')
+console.log(allChar)
+var alphUp = allChar.toUpperCase()
+let p1 = people.length
+let a1 = alphabet.length
+console.log(p1)
+console.log(a1)
+array.forEach(people => {
+    for(i = 0; i < people[i]; i++)
+    console.log(people[i])
+    
+})
