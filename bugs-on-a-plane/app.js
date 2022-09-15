d00f0cce82f97d8c07b73630e623f7a2c428d05d
Sharon Brown <sbsharonbrown311@gmail.com>
2022-09-15 11:00:41 -0400

    Bugs on a plane assignment working and completed

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -1,48 +1,34 @@
-const form = document.getElementById("airlineForm");
-const buttonSubmit = document.getElementById("buttonSubmit");
-console.log(airlineForm)
-airlineForm.addEventListener('submit', (event) => {
-  event.preventDefault()
-  console.log('clicked')
-})
-function formAlert (event){
-  event.preventDefault()
-  let firstName = form.elements.firstName.value;
-     firstName.value = ''
-    // console.log(firstName.value)
-  let lastName = form.elements.lastName.value;
-  let age = form.elements.age.value;
-  let gender = form.elements.gender.value;
-  let travelLocation = form.elements.travelLocation.value;
-  console.log(travelLocation)
-  var diet = ['vegan', 'gluten', 'paleo'];
-  if (form.elements.diet[0].checked) {
-    diet.push(document.querySelector("vegan").value);
-  }
-  else if (form.elements.diet[1].checked) {
-    diet.push(document.querySelector("gluten").value);
-  }
-  else if (form.elements.diet[2].checked) {
-    diet.push(document.querySelector("paleo").value);
-  }
-  console.log(form.elements.value)
-  alert(
-    "First Name: " +
-    firstName +
-    " Last Name: " +
-    lastName +
-    " Age: " +
-    age +
-    " Gender: " +
-    gender +
-    " Travel Location: " +
-    travelLocation +
-    " Diet: " +
-    diet +
-    " Awesome, now if you die, it won't be an accident.."
-    
-  );
-
-}
+const form = document.getElementById('airline-form');
+//const submit = document.getElementById('submit');
 
 
+form.addEventListener('submit' , formAlert )
+function formAlert(event){
+    var firstName = form.elements['first-name'].value;
+    //console.log(firstName)
+     var  lastName = form.elements['last-name'].value;
+    //console.log(lastName)
+    var age = form.elements['age'].value;
+    //console.log(age)
+    var gender = form.elements['gender'].value;
+    //console.log(gender)
+    var location = form.elements['travel-location'].value;
+    //console.log(location)
+    let diet = [];
+  //form.addEventListener("submit" , formAlert);
+    if (form.elements['vegan'].checked){
+        diet.push(document.getElementById('vegan').value);
+    }
+    if(form.elements['gluten'].checked){
+        diet.push(document.getElementById('gluten').value)
+    }
+    if(form.elements['paleo'].checked){
+        diet.push(document.getElementById('paleo').value)
+    };
+  //all if statement used in case of multiple responses of diet
+    alert("First Name: " + firstName + "\nLast Name: "+ lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+
+};
+
+
+///finally it works
