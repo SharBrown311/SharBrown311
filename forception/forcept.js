5b775a91e92c8b4751f30b86abee73a8daabb18c
Sharon Brown <sbsharonbrown311@gmail.com>
2022-09-15 09:50:29 -0400

    Forception Assignment

--- forception/forcept.js
+++ forception/forcept.js
@@ -1,19 +1,38 @@
 
-
-var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
-
-
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-    alphabet = alphabet.toUpperCase()
-console.log(alphabet);
-    alphabet = alphabet.split('')
-console.log(alphabet)
-var result = []
+    //alphabet = alphabet.toUpperCase()
+    //alphabet = alphabet.split('')
 
     
-function forception(people , alphabet){
-    for( let index = 0; index < people.length; ++ index){
-        var result = people[index]
-        console.log (result)
+// function forception(people , alphabet){
+//     
+//     var result = [];
+//     for (var i = 0 ; i < people.length; i ++){
+//         result.push(people[i].concat(':'));
+//         for(var j = 0; j <alphabet.length; j ++){
+//             result.push(alphabet.toUpperCase().split('')[j])
+//         }
+//     }
+// return result;
+        
+// }
+// console.log(result);
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var resultOfForception = function(people , alphabet){
+var result = [];
+for(var i = 0; i < people.length; i ++){
+    result.push(people[i].concat(":"));
+    for(var j = 0; j < alphabet.length; j ++){
+        result.push(alphabet.toUpperCase().split("")[j]);
     }
 }
+return result
+}
+console.log(resultOfForception(people , alphabet))
+
+//output : // [object Array] (135)
+//["Jon:","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+//"Jacob:","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+//"Jingle:","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+//"Heimer:","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+//"Schmidt:","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
