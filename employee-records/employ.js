860580d6bea7129c42753e9442f63d13a5e138f1
Sharon Brown <sbsharonbrown311@gmail.com>
2022-09-15 11:20:52 -0400

    Employee Records Assignment Completed

--- employee-records/employ.js
+++ employee-records/employ.js
@@ -1,24 +1,26 @@
 let employees = [];
 const printEmployeeForm = employees
 
-function Employees(name, title, salary, status) {//formal arguments
+function Employee(name, title, salary) {//formal arguments
   this.name = name;
   this.title = title;
   this.salary = salary;
-  this.status = status;
-  
-Employees.prototype.status = function(){
-  console.log(this.status)
+  this.status = "Full Time";
 }
-function printEmployeeForm(employees){[
-    this.employee1 = {name:"Thomas Jefferson",title: "Money Model" ,salary:"$" + 20 ,status: "Full-time"},
-    this.employee2 = {name:'George Washington' ,title:'Cherry Tree Chopper',salary: 7 + 'stacks of wood',status: 'Contract'},
-    this.employee3 = {name:"Benjamin Franklin" ,  title:"Scientist", salary: 40, status: 'Part-time'}]
-    employees = [this.employee1,this.employee2,this.employee3];
-    printEmployeeForm(employees)
+Employee.prototype.printEmployeeForm = function(){
+  console.log(this.name + ' \n' + this.title + '\n' + this.salary + '\n' + this.status)
+}
+var emp1 = new Employee('Abraham Lincoln' , 'Stand Up Comedian', 50.00 + '/n per joke about Frederick Douglas')
+var emp2 = new Employee('Thomas Jefferson' , "Nickel Model" , .05 + 'cents' )
+var emp3 = new Employee('Woodrow Wilson' , "Political Scientist" , 55000)
+emp2.status = 'Contract'
+emp3.status = 'Part Time'
 
  
-}
-printEmployeeForm()
 
-}
+emp1.printEmployeeForm()
+emp2.printEmployeeForm()
+emp3.printEmployeeForm()
+employees.push(emp1 , emp2, emp3)
+console.log(employees)
+
