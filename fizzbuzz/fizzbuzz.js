e35ac6f72552fdb1bc450575b12a3d0a7b396ac6
Sharon Brown <sbsharonbrown311@gmail.com>
2022-10-03 13:02:31 -0400

    FizzBuzz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -2,130 +2,117 @@
 //for multiples of 5 print "Buzz"
 //numbers which are multiples of both 3 and 5 print 'FizzBuzz'
 let i;
-function fizzBuzz(){
-for (i = 1 ; i <= 100; i ++){
-    if(i%15 == 0){
-        //both 3 and 5
-        console.log('FizzBuzz')
-    }else if((i % 3) == 0){
-        //multiples of 3
-        console.log('Fizz')
-    }else if((i % 5) == 0){
-        //multiples of 5
-        console.log('Buzz')
-    }else{
-        console.log(i)
+function fizzBuzz() {
+  for (i = 1; i <= 100; i++) {
+    if (i % 15 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
     }
+  }
 }
-console.count('Fizz')
-console.count('Buzz')
-console.count('FizzBuzz')
-}
-//output: 
-/* Console
-Clear
-
-1
-2
-"Fizz"
-4
-"Buzz"
-"Fizz"
-7
-8
-"Fizz"
-"Buzz"
-11
-"Fizz"
-13
-14
-"FizzBuzz"
-16
-17
-"Fizz"
-19
-"Buzz"
-"Fizz"
-22
-23
-"Fizz"
-"Buzz"
-26
-"Fizz"
-28
-29
-"FizzBuzz"
-31
-32
-"Fizz"
-34
-"Buzz"
-"Fizz"
-37
-38
-"Fizz"
-"Buzz"
-41
-"Fizz"
-43
-44
-"FizzBuzz"
-46
-47
-"Fizz"
-49
-"Buzz"
-"Fizz"
-52
-53
-"Fizz"
-"Buzz"
-56
-"Fizz"
-58
-59
-"FizzBuzz"
-61
-62
-"Fizz"
-64
-"Buzz"
-"Fizz"
-67
-68
-"Fizz"
-"Buzz"
-71
-"Fizz"
-73
-74
-"FizzBuzz"
-76
-77
-"Fizz"
-79
-"Buzz"
-"Fizz"
-82
-83
-"Fizz"
-"Buzz"
-86
-"Fizz"
-88
-89
-"FizzBuzz"
-91
-92
-"Fizz"
-94
-"Buzz"
-"Fizz"
-97
-98
-"Fizz"
-"Buzz" */
-
-
-
-//tally of outputs:
+fizzBuzz();
+// //output:
+// 1
+// 2
+// "Fizz"
+// 4
+// "Buzz"
+// "Fizz"
+// 7
+// 8
+// "Fizz"
+// "Buzz"
+// 11
+// "Fizz"
+// 13
+// 14
+// "FizzBuzz"
+// 16
+// 17
+// "Fizz"
+// 19
+// "Buzz"
+// "Fizz"
+// 22
+// 23
+// "Fizz"
+// "Buzz"
+// 26
+// "Fizz"
+// 28
+// 29
+// "FizzBuzz"
+// 31
+// 32
+// "Fizz"
+// 34
+// "Buzz"
+// "Fizz"
+// 37
+// 38
+// "Fizz"
+// "Buzz"
+// 41
+// "Fizz"
+// 43
+// 44
+// "FizzBuzz"
+// 46
+// 47
+// "Fizz"
+// 49
+// "Buzz"
+// "Fizz"
+// 52
+// 53
+// "Fizz"
+// "Buzz"
+// 56
+// "Fizz"
+// 58
+// 59
+// "FizzBuzz"
+// 61
+// 62
+// "Fizz"
+// 64
+// "Buzz"
+// "Fizz"
+// 67
+// 68
+// "Fizz"
+// "Buzz"
+// 71
+// "Fizz"
+// 73
+// 74
+// "FizzBuzz"
+// 76
+// 77
+// "Fizz"
+// 79
+// "Buzz"
+// "Fizz"
+// 82
+// 83
+// "Fizz"
+// "Buzz"
+// 86
+// "Fizz"
+// 88
+// 89
+// "FizzBuzz"
+// 91
+// 92
+// "Fizz"
+// 94
+// "Buzz"
+// "Fizz"
+// 97
+// 98
+// "Fizz"
+// "Buzz"
