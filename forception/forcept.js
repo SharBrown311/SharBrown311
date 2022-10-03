06dd17dffc03b500b989183a9312af76a6f72df6
Sharon Brown <sbsharonbrown311@gmail.com>
2022-10-03 13:07:14 -0400

    Forception

--- forception/forcept.js
+++ forception/forcept.js
@@ -1,24 +1,7 @@
-
-    //alphabet = alphabet.toUpperCase()
-    //alphabet = alphabet.split('')
-
-    
-// function forception(people , alphabet){
-//     
-//     var result = [];
-//     for (var i = 0 ; i < people.length; i ++){
-//         result.push(people[i].concat(':'));
-//         for(var j = 0; j <alphabet.length; j ++){
-//             result.push(alphabet.toUpperCase().split('')[j])
-//         }
-//     }
-// return result;
-        
-// }
-// console.log(result);
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var resultOfForception = function(people , alphabet){
+ 
+    function forception(people , alphabet){
 var result = [];
 for(var i = 0; i < people.length; i ++){
     result.push(people[i].concat(":"));
@@ -28,7 +11,7 @@ for(var i = 0; i < people.length; i ++){
 }
 return result
 }
-console.log(resultOfForception(people , alphabet))
+console.log(forception(people, alphabet))
 
 //output : // [object Array] (135)
 //["Jon:","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
