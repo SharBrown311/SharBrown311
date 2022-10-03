41ab56d5cfd80e10263635d991b748cd4e646adc
Sharon Brown <sbsharonbrown311@gmail.com>
2022-10-03 14:57:32 -0400

    Mom's Shopping List

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,59 +1,43 @@
-var button = document.getElementById('submitItem')
-submitItem.innerHTML = 'Submit'//submit button
-var list = document.getElementById('list')//ul
+var button = document.getElementById("submitItem");
+submitItem.innerHTML = "Submit"; //submit button
+var list = document.getElementById("list"); //ul
+var form = document.getElementById("listForm");
 
-                                                            
-var form = document.getElementById('listForm')
-// 1. console.log contents of variables that store returns from dom manipulation 
-console.log("form var: ", form)
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    addItem()
-})//form element
-
-// var testFn = function() {
-//     // e.preventDefault()
-//     console.log("The form was submitted")
-// }
-
-// var exampleForm = document.getElementById("exampleForm")
-// console.log(exampleForm)
+console.log("form var: ", form);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addItem();
+});
 
 var addItem = function () {
+  var text = txtValu.value;
+  txtValu.value = "";
+  var li = document.createElement("li");
+  li.innerHTML = text;
+  var br = document.createElement("br");
+  list.appendChild(li);
+  li.append(br);
 
-    var text = txtValu.value;
-    txtValu.value = "";
-    var li = document.createElement('li');
-    li.innerHTML = text;
-    var br = document.createElement('br')
-    list.appendChild(li);
-    li.append(br)
+  var buttonDelete = document.createElement("button");
+  buttonDelete.addEventListener("click", deleteItem);
 
-    var buttonDelete = document.createElement('button')
-    buttonDelete.addEventListener('click' , deleteItem)
-
-    // named function definition
-    function deleteItem(e){
-        console.log("delete was called")
-        li.remove()
-    }
-    var buttonEdit = document.createElement('button')
-    buttonDelete.innerHTML = "X"
-    buttonEdit.innerHTML = 'Edit'
-    buttonDelete.style.height= '30px'
-        buttonDelete.style.width = '43.09px'
-        buttonDelete.style.backgroundColor = 'red'
-        buttonEdit.style.height = '30px'
-        buttonEdit.style.width = '43.09px'
-        buttonEdit.style.backgroundColor = 'blue'
-    li.appendChild(buttonDelete)
-    li.appendChild(buttonEdit)
-}
-
-function editItem(e){
-    let item = e.target.innerHTML
-
-let itemInput = document.createElement('input')
-itemInput.type = 'text'
-itemInput.value = item
-}
+  function deleteItem(e) {
+    console.log("delete was called");
+    li.remove();
+  }
+  var buttonEdit = document.createElement("button");
+  buttonDelete.innerHTML = "X";
+  buttonEdit.innerHTML = "Edit";
+  buttonDelete.style.height = "30px";
+  buttonDelete.style.width = "43.09px";
+  buttonDelete.style.backgroundColor = "red";
+  buttonEdit.style.height = "30px";
+  buttonEdit.style.width = "43.09px";
+  buttonEdit.style.backgroundColor = "blue";
+  li.appendChild(buttonDelete);
+  li.appendChild(buttonEdit);
+  buttonEdit.addEventListener("click", function () {
+    li.contentEditable = true;
+    li.style.backgroundColor = "blue";
+  });
+};
