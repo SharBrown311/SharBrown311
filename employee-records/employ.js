ffda6366771b694b103cefbe205d616fa5826801
Sharon Brown <sbsharonbrown311@gmail.com>
2022-10-03 12:56:11 -0400

    Employee Records

--- employee-records/employ.js
+++ employee-records/employ.js
@@ -10,8 +10,8 @@ function Employee(name, title, salary) {//formal arguments
 Employee.prototype.printEmployeeForm = function(){
   console.log(this.name + ' \n' + this.title + '\n' + this.salary + '\n' + this.status)
 }
-var emp1 = new Employee('Abraham Lincoln' , 'Stand Up Comedian', 50.00 + '/n per joke about Frederick Douglas')
-var emp2 = new Employee('Thomas Jefferson' , "Nickel Model" , .05 + 'cents' )
+var emp1 = new Employee('Abraham Lincoln' , 'Stand Up Comedian', 50.00)
+var emp2 = new Employee('Thomas Jefferson' , "Nickel Model" , .05)
 var emp3 = new Employee('Woodrow Wilson' , "Political Scientist" , 55000)
 emp2.status = 'Contract'
 emp3.status = 'Part Time'
@@ -23,4 +23,3 @@ emp2.printEmployeeForm()
 emp3.printEmployeeForm()
 employees.push(emp1 , emp2, emp3)
 console.log(employees)
-
