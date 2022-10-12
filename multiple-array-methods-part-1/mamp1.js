5126d0e3cd9cd061e8f1f41df3239d7af676f109
Sharon Brown <sbsharonbrown311@gmail.com>
2022-10-12 13:51:35 -0400

    Multiple Array Methods Part 1

---
+++ multiple-array-methods-part-1/mamp1.js
@@ -0,0 +1,67 @@
+
+ const peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+
+function sortedOfAge(arr){
+     const sorted = arr.filter(arr => arr.age > 18)
+        const alphaSorted = arr.sort(function(a,b){
+            a.firstName - b.lastName
+        })
+        return sorted + alphaSorted
+ }
+ 
+ 
+ console.log(sortedOfAge(peopleArray));
+ 
+ /*
+ Output:
+ [
+     "<li>Kyle Mooney is 27</li>",
+     "<li>Sarah Palin is 47</li>",
+     "<li>Rick Sanchez is 78</li>",
+     "<li>Morty Smith is 29</li>",
+     "<li>Lev Tolstoy is 82</li>"
+ ]
+ */
+
+ 
+
+
+
+
