fdd448417501a91f70accb2bca606a6345b0a1f2
Sharon Brown <sbsharonbrown311@gmail.com>
2023-01-10 09:42:48 -0500

    changes

---
+++ Level-3/Challenges/hero-page/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './assets/Navbar'
+import Hero from './assets/Hero'
+import Card from './assets/Card'
+import './App.css'
+
+export default function App() {
+
+
+  return (
+    <div className="App">
+       <Navbar  />
+       <Hero />
+       <Card />
+   </div>
+  )
+}
+
+
---
+++ Level-3/Challenges/hero-page/src/assets/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img className= "nav--logo" src="https://tse3.explicit.bing.net/th?id=OIP.UYcuD7k4zuPbbWq9YqfLLAHaCU&pid=Api&P=0" alt="airbnb logo"/>
+        </nav>
+    )
+}
